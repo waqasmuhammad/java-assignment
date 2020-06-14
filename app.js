f2944a3a9e0ced335fdcb8f65e595cb92f688c75
waqasmuhammad <65039330+waqasmuhammad@users.noreply.github.com>
2020-06-14 16:53:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,279 @@
+/*  
+   chapter 1
+
+
+   alert("Error! Please Enter A Valid Password");
+alert("Welcome To JS Land \n Happy Coding");
+alert("Welcome To JS Land");
+alert(" Happy Coding");
+alert("Hello... I can learn  JS Through My Browser Console");*/
+
+
+
+
+
+
+
+
+     /*   chapter 2
+
+
+ 
+var username;
+var myname = "Muhammad Waqas";
+var message = "Hello World";
+alert(message);
+
+var name = "Waqas";
+var age = "15 Years old";
+var power = "Create Mobile Applocation Developement";
+alert(name);
+alert(age);
+alert(power);
+
+var pizza = "PIZZA \n PIZZ \n PIZ  \n PI \n P";
+alert(pizza);
+
+
+var email = "mr.waqassial7862@gmail.com";
+alert("My Email Address is" + " " +  email);
+
+var book = "I am Trying to learn from a smarter way to learn JavaScript" ;
+alert(book);
+
+document.write("YES! I can write HTML Content Through JavaScript");
+
+var abc = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(abc);*/
+          
+
+   /*       chapter 3
+
+
+var age = " I am 15 Years Old";
+alert(age);
+
+var N ="14";
+alert("You Have Visit This Site" + " " + N + "Times");
+
+var birthYear = "24-10-1999";
+document.write("Your Birth Year is " + " " + birthYear);
+
+
+var visit = "John Doe";
+var title = "T-Shirt";
+var quan = "5";
+document.write( visit +  " " + "Ordered"  +  " "  + quan +  " "  + title +  " "  + "On XYZ.com");
+                          
+*/
+
+/*
+                        chapter 4
+
+var ali,aliya,iqra;
+//var 3aliya, 1'ali, r*ibrar, $Tayyab 192,
+var ali = "numbers";
+var asad= "letters";
+var ibrar = "$" ;
+var hasnat  = "_" ;
+var amir  = " sensitive";
+var aqib = "Keywords";
+document.write("<h1> Rules for naming JS variables </h1>");
+document.write("<br />");
+document.write("Variable names can only contain" + " " + ali + " " + ibrar + " " + hasnat + "For example $my_1stVariable");
+document.write("<br />");
+document.write("Variables must begin with a " + " " + asad2 + " " + ibrar+ " " + hasnat + "  For example $name, _name or name" );
+document.write("<br />");
+document.write("Variable names are case" + " " + amir);
+document.write("<br />");
+document.write("Variable names should not be JS" + " " + amir);*/
+
+ /*                                chapter 5
+
+
+
+var first= 3;
+var second = 5;
+var add = first +second;
+var sub = first -second;
+var mult = first *second;
+var mod = first % second;
+document.write("Sum of 3 + 5 is " + " " + add);
+document.write("<br />");
+document.write("Subtract of 3 - 5 is " + " " + sub);
+document.write("<br />");
+document.write("Multiply of 3 * 5 is " + " " + mult);
+document.write("<br />");
+document.write("Modulus of 3 % 5 is " + " " + mod);
+document.write("<br />");
+
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+var ali = 0 ;
+document.write(" Value after variable declaration is " + " " + ali);
+document.write("<br />");
+var ahmad = 5;
+ahmad++;
+document.write(" Value after variable declaration is " + " " + ahmad);
+document.write("<br />"); 
+
+ahmad = ahmad + 7;
+document.write(" Value after variable declaration is " + " " + ahmad);
+document.write("<br />");
+ahmad--
+document.write(" Value after variable declaration is " + " " + ahmad);
+document.write("<br />");
+ahmad = ahmad%3;
+document.write(" Value after variable declaration is " + " " + ahmad);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+var price = 600;
+var tickets = 5;
+
+document.write("Total Cost to Buy " + tickets +   " Tickets to a Movie is " + price*tickets );
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+var first=4;
+var second;
+for(second=1;second<=10;second++ ){
+	document.write(first +" x" + second + " =" + first*second );
+	document.write("<br />");
+}
+
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+var cels= 25;
+var faren= 71; 
+var temp =  (faren-32)*5/9;
+var temp2 = (cels *9/5)-32;
+document.write(cels + "is" + temp);
+document.write("<br />");
+document.write("<br />");
+document.write(faren + "is" + temp2);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+var item1 =650;
+var quant = 3;
+var item2 = 100;
+var quant2= 7;
+var ship = 100;
+var total = (item1*quant)+(item2*quant2)+ship;
+
+document.write("Price of item1 is " + item1);
+document.write("<br />");
+document.write("Quantity of item1 is" + quant);
+document.write("<br />");
+document.write("Price  of item2 is "+ item2);
+document.write("<br />");
+document.write("Quantity of item2 is " + item2);
+document.write("<br />");
+document.write("Shippment Charges is " + ship);
+document.write("<br />");
+document.write("Total is " + total);
+document.write("<br />");
+
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+var total= 980;
+var obt = 804;
+var percent = obt/total*100;
+document.write("<h1> Total Marks is </h1>" + total);
+document.write("<h1> Obtained Marks is </h1>" + obt);
+document.write("<h1>  Percentage is  </h1>" + percent);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+
+var usd = 104.80*10;
+var saud = 28*25;
+var total=usd+saud;
+document.write("<h1> Currency in Pakistan is  </h1>" +  total);
+
+
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+var number = 0; 
+var add = number+5;
+var multi = add*10;
+var div = multi/2;
+document.write("Total Answer is " + " " +  div);
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+var current = 2020;
+var birth = 2000;
+var age = current-birth;
+document.write("<h1> Your Current Age is  </h1>" + " " + age);
+
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+document.write("<br />");
+
+
+var value2= 20;
+var circum = 2*3.14*value2;
+var area= 3.14*value2*value2;
+document.write("Radius of Circle is " + " " + value2);
+document.write("<br />");
+document.write("Area of Circumference is " + " " + circum);
+document.write("<br />");
+document.write("Area of Square is " +  " " +  area);
+
+document.write("<br />");
+
+document.write("<br />");
+
+document.write("<br />");
+
+document.write("<br />");
+
+document.write("<h1> The Lifetime Supply Calculator </h1>");
+var snack = "Choclate Chip";
+var age1= 15;
+var maxage = 65;
+var snackp = 3;
+document.write("You Will Need " + age1*10 + " " + snack  +  " untill you have the age of " + maxage);
+
+*/
